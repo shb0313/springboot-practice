8eded8ced30c5fa8bcab9cf77a88a93cb9648a8f
shb7815 <shb7815@gmail.com>
2023-02-15 13:14:04 +0900

    springboot-ex

---
+++ springboot-ex/src/test/java/com/douwone/springbootex/SpringbootExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douwone.springbootex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
