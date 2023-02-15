320f2d31357d63baa56c0189faa624a38a280774
shb7815 <shb7815@gmail.com>
2023-02-15 15:56:21 +0900

    springboot-myapplication

---
+++ springboot-myapplication/src/main/java/ex04/MyApplication.java
@@ -0,0 +1,24 @@
+package ex04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/*
+ * @SpringBootApplication 메타 어노테이션
+ *  + @SpringBootConfiguration : cf) ex01, ex02
+ *  + @ComponentScan : ex04 하부 패키지를 스캐닝한다. cf) ex03
+ *  + @EnableAutoConfiguration : 
+ *  	1. MVC, AOP, Security, JPA 등을 자동 "default"(**미새 설정 필수**) 설정
+ *  	2. 발견된 Starter Dependency(Library) 기반으로 설정한다.
+ */
+
+
+@SpringBootApplication
+public class MyApplication {
+	public static void main(String[] args) {
+		// 자원정리
+		try( ConfigurableApplicationContext ac = SpringApplication.run(MyApplication.class, args) ){}
+	
+	}
+}
